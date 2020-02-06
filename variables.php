5f061c70076b69edb7a1d8315139f1c85c534a41
Laura J Defibaugh <ldefibaugh9988@gmail.com>
2020-02-06 09:56:39 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang = "en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>	
+<body>
+	<?php //Script 2.3 - variables.php
+	
+	//personal info
+	$street = "18052 N. Black Canyon Hwy.";
+	$city = "Phoenix";
+	$state = "AZ";
+	$zip = 85053;
+	$fname = "Laura";
+	$lname = "Defibaugh";
+	$phone = "602-284-9760";
+		
+	//Print info
+	print "<p>The personal information is:<br>$fname $lname<br>$phone<br>$street<br>$city $state $zip</p>";
+		
+	?>
+</body>
+</html>
