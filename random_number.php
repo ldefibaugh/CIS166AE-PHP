bfff6b7191b1de12feed6d4b38098bfe51a28981
Laura J Defibaugh <ldefibaugh9988@gmail.com>
2020-02-19 18:52:57 -0700

    Add files via upload

---
+++ random_number.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Random Number</title>
+</head>
+
+<body>
+	
+	<?php
+	echo(rand(10,100)."<br>");
+	?>
+	
+</body>
+</html>
