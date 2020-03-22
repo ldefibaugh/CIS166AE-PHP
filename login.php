1e47293b92d25977b515e366096b92f05777050b
Laura J Defibaugh <ldefibaugh9988@gmail.com>
2020-03-22 14:05:31 -0700

    Add files via upload

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 
 // Set the page title and include the header file:
 define('TITLE', 'Login');
-include('templates/header.html');
+include('Templates/header.html');
 
 // Print some introductory text:
 print '<h2>Login Form</h2>
@@ -16,20 +16,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if ( (!empty($_POST['email'])) && (!empty($_POST['password'])) ) {
 
 		if ( (strtolower($_POST['email']) == 'me@example.com') && ($_POST['password'] == 'testpass') ) { // Correct!
-			print '<p class="text--success">You are logged in!<br>Now you can blah, blah, blah...</p>';
+
+			//print '<p class="text--success">You are logged in!<br>Now you can blah, blah, blah...</p>';
+			ob_end_clean();
+			header('Location: welcome.php');
+			exit();
+
 		} else { // Incorrect!
+
 			print '<p class="text--error">The submitted email address and password do not match those on file!<br>Go back and try again.</p>';
+
 		}
+
 	} else { // Forgot a field.
+
 		print '<p class="text--error">Please make sure you enter both an email address and a password!<br>Go back and try again.</p>';
+
 	}
+
 } else { // Display the form.
+
 	print '<form action="login.php" method="post" class="form--inline">
 	<p><label for="email">Email Address:</label><input type="email" name="email" size="20"></p>
 	<p><label for="password">Password:</label><input type="password" name="password" size="20"></p>
 	<p><input type="submit" name="submit" value="Log In!" class="button--pill"></p>
 	</form>';
+
 }
 
-include('templates/footer.html'); // Need the footer.
+include('Templates/footer.html'); // Need the footer.
 ?>
