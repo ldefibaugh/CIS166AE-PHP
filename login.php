d9aa16786a6d7758a7b6a065a9eab05c1929023f
Laura J Defibaugh <ldefibaugh9988@gmail.com>
2020-03-29 13:27:43 -0700

    Add files via upload

--- login.php
+++ login.php
@@ -11,37 +11,32 @@ print '<h2>Login Form</h2>
 
 // Check if the form has been submitted:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
 	// Handle the form:
 	if ( (!empty($_POST['email'])) && (!empty($_POST['password'])) ) {
-
 		if ( (strtolower($_POST['email']) == 'me@example.com') && ($_POST['password'] == 'testpass') ) { // Correct!
-
+			
+			//Do Session Stuff
+			session_start();
+			$_SESSION['email'] = $_POST['email'];
+			$_SESSION['loggedin'] = time();
+			
 			//print '<p class="text--success">You are logged in!<br>Now you can blah, blah, blah...</p>';
 			ob_end_clean();
 			header('Location: welcome.php');
 			exit();
-
 		} else { // Incorrect!
-
 			print '<p class="text--error">The submitted email address and password do not match those on file!<br>Go back and try again.</p>';
-
 		}
-
 	} else { // Forgot a field.
-
 		print '<p class="text--error">Please make sure you enter both an email address and a password!<br>Go back and try again.</p>';
-
 	}
 
 } else { // Display the form.
-
 	print '<form action="login.php" method="post" class="form--inline">
 	<p><label for="email">Email Address:</label><input type="email" name="email" size="20"></p>
 	<p><label for="password">Password:</label><input type="password" name="password" size="20"></p>
 	<p><input type="submit" name="submit" value="Log In!" class="button--pill"></p>
 	</form>';
-
 }
 
 include('Templates/footer.html'); // Need the footer.
