69854d32aa7d6d5c683660650db929babe0c804b
Laura J Defibaugh <ldefibaugh9988@gmail.com>
2020-04-11 11:01:43 -0700

    Add files via upload

---
+++ mkdir_test.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Directory</title>
+</head>
+
+	
+<body>
+	<h1>Create Directory</h1>
+	
+<?php
+mkdir("/xampp/htdocs/users/test");
+?>
+	
+</body>
+</html>
